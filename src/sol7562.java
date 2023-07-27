b606d151a8891e37612686ab5f6181584acf2db0
okrang <greenhome04@naver.com>
2023-07-27 15:02:41 +0900

    sol7562 bfs solved successg

--- src/sol7562.java
+++ src/sol7562.java
@@ -10,42 +10,46 @@ import java.util.Queue;
 public class sol7562 {
     public static int [][] graph;
     public static int [][] distance;
+    public static int chess;
+    public static int y,x,a,b;
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n=Integer.parseInt(br.readLine());
-        Queue<int[]> que=new LinkedList();
+        Queue<int[]> que;
         for(int i=0;i<n;i++){
-
-            int chess=Integer.parseInt(br.readLine());
+            que=new LinkedList<>();
+            chess=Integer.parseInt(br.readLine());
             graph=new int [chess][chess];
             distance=new int [chess][chess];
 
 
             st=new StringTokenizer(br.readLine());
-            int y=Integer.parseInt(st.nextToken());
-            int x=Integer.parseInt(st.nextToken());
+            y=Integer.parseInt(st.nextToken());
+            x=Integer.parseInt(st.nextToken());
             //graph[y][x]=1;
             que.offer(new int[]{y,x});
+            graph[y][x]=1;
 
             st=new StringTokenizer(br.readLine());
-            int a=Integer.parseInt(st.nextToken());
-            int b=Integer.parseInt(st.nextToken());
+            a=Integer.parseInt(st.nextToken());
+            b=Integer.parseInt(st.nextToken());
             //graph[a][b]=1;
 
             while(!que.isEmpty()){
                 int[] point=que.poll();
-                graph[point[0]][point[1]]=1;
+
                 int[] dy={-2,-1,1,2,2,1,-1,-2};
                 int[] dx={1,2,2,1,-1,-2,-2,-1};
                 if(point[0]==a&&point[1]==b){
-                    return;
+                    break;
                 }
                 for(int k=0;k<8;k++){
                     int cy=point[0]+dy[k];
                     int cx=point[1]+dx[k];
                     if(cy>=0&&cx>=0&&cy<chess&&cx<chess&&graph[cy][cx]!=1){
                         que.offer(new int[]{cy,cx});
+                        graph[cy][cx]=1;
                         distance[cy][cx]=distance[point[0]][point[1]]+1;
                     }
                 }
@@ -57,14 +61,6 @@ public class sol7562 {
             }
 
             System.out.println(distance[a][b]);
-
-
-
-
-
-
-
-
 
 
 
