8cb67558455af91fb9b90538b9f35037eceab259
okrang <greenhome04@naver.com>
2024-01-15 23:07:09 +0900

    pkg178871 hashing success coding using hashmap_list_keyset

---
+++ src/Hashing/prg178871.java
@@ -0,0 +1,31 @@
+package Hashing;
+
+public class prg178871 {
+    import java.util.*;
+    class Solution {
+        public String[] solution(String[] players, String[] callings) {
+            String[] answer = {};
+            answer=new String[players.length];
+            Map<String,Integer> map=new HashMap<>();
+            Map<Integer,String>map2=new HashMap<>();
+            for(int i=0;i<players.length;i++){
+                map.put(players[i],i);
+                map2.put(i,players[i]);
+            }
+            for(int i=0;i<callings.length;i++){
+                int order=map.get(callings[i]);
+                String reverse=map2.get(order-1);
+                map.put(callings[i],order-1);
+                map.put(reverse,order);
+                map2.put(order,reverse);
+                map2.put(order-1,callings[i]);
+            }
+            List<Integer> keyset=new ArrayList<>(map2.keySet());
+            Collections.sort(keyset);
+            for(int i=0;i<keyset.size();i++){
+                answer[i]=map2.get(keyset.get(i));
+            }
+            return answer;
+        }
+    }
+}
