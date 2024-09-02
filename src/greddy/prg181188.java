257564b29583c1545b65d8248131fac356d3bc14
okrang <greenhome04@naver.com>
2024-09-03 00:00:19 +0900

    prg solving

--- src/greddy/prg181188.java
+++ src/greddy/prg181188.java
@@ -2,17 +2,23 @@ package greddy;
 import java.util.*;
 public class prg181188 {
 
+    
     class Solution {
         public int solution(int[][] targets) {
             int answer = 0;
             int end=0;
-            Arrays.sort(targets,((o1,o2)->o1[0]-o2[0]));
+            Arrays.sort(targets,(o1,o2)->{
+                if(o1[0]==o2[0]){
+                    return o2[1]-o1[1];
+                }else{
+                    return o1[0]-o2[0];
+                }
+            });
             //System.out.println(Arrays.deepToString(targets));
             for(int i=0;i<targets.length;i++){
                 if(targets[i][0]>=end){
-                    end=targets[i][1];
+                    end=targets[0][1];
                     answer++;
-                    System.out.println(end);
                 }
             }
             return answer;
